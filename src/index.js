b14e9d52e50416df1fc933ee51d13ee263911848
joshmrtn <10765818+joshmrtn@users.noreply.github.com>
2025-09-18 16:58:36 -0400

    feat: get basic CLI input from user

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-console.log("Hello world!");
-export {};
+import { createInterface } from 'node:readline';
+import { exit, stdin, stdout } from 'node:process';
+const rl = createInterface({
+    input: stdin,
+    output: stdout
+});
+rl.setPrompt('Enter your command: ');
+rl.prompt();
+rl.on('line', (line) => {
+    switch (line.trim()) {
+        case 'add':
+            console.log('you entered the add command');
+            break;
+        default:
+            console.log('Invalid command');
+            break;
+    }
+    rl.prompt();
+});
 //# sourceMappingURL=index.js.map
